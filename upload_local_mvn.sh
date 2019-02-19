13142b957b33aacfb2cadc6fbac115aaeab1fbe7
aaron <aaronlzxian@163.com>
2019-02-19 10:15:57 +0800

    rm upload local mvn

--- upload_local_mvn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn install:install-file -Dfile=/Users/aaron/Code/Java/Jinuo/ACE/local_jar/bean-utils-1.0.jar -DgroupId=com.dcb -DartifactId=bean-utils -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=/Users/aaron/Code/Java/Jinuo/ACE/local_jar/common-utils-1.0.jar -DgroupId=com.dcb -DartifactId=common-utils -Dversion=1.0 -Dpackaging=jar
