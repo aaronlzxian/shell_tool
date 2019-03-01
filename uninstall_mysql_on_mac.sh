002105fb8165fbbcf9ec370da12394f3bc7ce7bc
aaron <aaronlzxian@163.com>
2019-03-01 10:11:26 +0800

    rm uninstall_mysql_on_mac.sh

--- uninstall_mysql_on_mac.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo rm /usr/local/mysql
-sudo rm -rf /usr/local/mysql*
-sudo rm -rf /Library/StartupItems/MySQLCOM
-sudo rm -rf /Library/PreferencePanes/My*
-rm -rf ~/Library/PreferencePanes/My*
-sudo rm -rf /Library/Receipts/mysql*
-sudo rm -rf /Library/Receipts/MySQL*
-sudo rm -rf /private/var/db/receipts/com.mysql.*
